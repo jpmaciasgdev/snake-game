c02243d6637ec92e0abd76313d980bc5f7a4ef3f
jpmaciasgdev <jpmaciasg@gmail.com>
2023-12-24 01:12:51 -0500

    Avance

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// main.js
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let snake = [
+  { x: 150, y: 150 },
+  { x: 140, y: 150 },
+  { x: 130, y: 150 },
+  { x: 120, y: 150 },
+  { x: 110, y: 150 },
+];
+
+function drawSnakePart(snakePart) {
+  ctx.fillStyle = "#226f54";
+  ctx.strokeStyle = "#ffb703ff";
+  ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+  snake.forEach(drawSnakePart);
+}
+
+// Llama a tu función para dibujar la serpiente
+drawSnake();
